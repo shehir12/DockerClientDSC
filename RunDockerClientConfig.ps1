da701e8d0ccc7464466f3d17b19197b0c10f9867
Andrew Weiss <andrew.weiss@microsoft.com>
2014-06-30 14:55:21 -0400

    updated run config script

--- RunDockerClientConfig.ps1
+++ RunDockerClientConfig.ps1
@@ -5,10 +5,24 @@
    This script is used to enact the generated DSC configuration
    on the specified host. It collects the "root" user's credential and
    initates a CIM session. Upon completion, the CIM session is removed.
-.PARAMETER $Hostname
-   The node on which the Docker configuration should be enacted.
+.PARAMETER Hostname
+   The node on which the Docker configuration should be enacted. This parameter
+   may be used in conjunction with the ConfigurationData parameter provided that there
+   are no duplicate node names.
+.PARAMETER ConfigurationData
+   The path to the .psd1 configuration data file. CIM sessions will be
+   established to all nodes defined in the AllNodes array. This parameter
+   may be used in conjunction with the Hostname parameter provided that there
+   are no duplicate node names.
 .EXAMPLE
    .\RunDockerClientConfig.ps1 -Hostname "mgmt01.contoso.com"
+
+   Configures a CIM session to "mgmt01.contoso.com" and executes Start-DscConfiguration.
+
+
+   .\RunDockerClientConfig.ps1 -ConfigurationData ".\SampleConfigData.psd1"
+
+   Configures a CIM session for each node defined in the AllNodes array in the "SampleConfigData.psd1" file.
 .NOTES
    Ensure that the DockerClient.ps1 DSC configuration has been
    executed and a subsequent .mof file has been generated prior to
@@ -19,11 +33,23 @@
 
 param
 (
-    $Hostname
+    [string]$Hostname,
+    [string]$ConfigurationData
+
 )
 
 $cred = Get-Credential -UserName "root" -Message "Enter password"
 $options = New-CimSessionOption -UseSsl -SkipCACheck -SkipCNCheck -SkipRevocationCheck
-$session = New-CimSession -Credential $cred -ComputerName $Hostname -Port 5986 -Authentication basic -SessionOption $options
-Start-DscConfiguration -CimSession $session -Path .\DockerClient -Verbose -Wait
+if ($ConfigurationData) {
+    $data = Invoke-Expression -Command "$(Get-Content -Path $ConfigurationData)"
+    $nodes = $data.AllNodes.Nodename
+}
+
+if ($Hostname) {
+    $nodes += $Hostname
+}
+
+$session = New-CimSession -Credential $cred -ComputerName $nodes -Port 5986 -Authentication basic -SessionOption $options
+Start-DscConfiguration -CimSession $session -Path $PSScriptRoot\DockerClient -Verbose -Wait -Force
+
 $session | Remove-CimSession
