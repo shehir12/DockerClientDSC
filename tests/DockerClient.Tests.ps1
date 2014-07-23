51b767785bbbf831125845c55ab1798f0cba255e
Andrew Weiss <andrew.weiss@outlook.com>
2014-07-23 14:27:45 -0400

    add initial unit tests

---
+++ tests/DockerClient.Tests.ps1
@@ -0,0 +1,16 @@
+﻿$parent = Split-Path -Parent $PSScriptRoot
+. "$parent\DockerClient.ps1"
+
+Describe "DockerClient" {
+    Context "when Hostname and Configuration parameters are null" { 
+		It "should throw an exception" {
+            { DockerClient } | Should Throw
+        }
+
+        It "should throw a specific exception message" {
+            { DockerClient } | Should Throw "Hostname and/or ConfigurationData must be specified"
+        }
+	}
+}
+
+if (Test-Path $PSScriptRoot\DockerClient) { Remove-Item -Recurse $PSScriptRoot\DockerClient }
