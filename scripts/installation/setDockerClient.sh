c2e012cfb9fea1c319f133dbb34e0546ae4be59d
Andrew Weiss <andrew.weiss@outlook.com>
2014-07-23 14:27:11 -0400

    fixed installation script typo

--- scripts/installation/setDockerClient.sh
+++ scripts/installation/setDockerClient.sh
@@ -4,15 +4,14 @@
 
 if [[ $(dpkg-query -W -f='${Status}' lxc-docker 2>/dev/null | grep -c "ok installed") -eq 0 ]]
 then
-	[ -e /usr/lib/apt/methods/https ] || {
+		[ -e /usr/lib/apt/methods/https ] || {
         apt-get update
         apt-get install apt-transport-https
-    }
-    
-    apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
-    sh -c "echo deb https://get.docker.io/ubuntu docker main /etc/apt/sources.list.d/docker.list"
+		}
+
+		apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+    sh -c "echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
     apt-get update
     apt-get install -y lxc-docker
-    sed -i '$acomplete -F _docker docker' /etc/bash_completion.d/docker.io
     exit 0
 fi
